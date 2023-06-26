d501121f5164455c6b478ab793993f93ad614d67
Matt Maher <matt.maher@sony.com>
2023-06-26 16:45:21 -0700

    Initial Commit

---
+++ hp-trivia/hp_triviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  hp_triviaApp.swift
+//  hp-trivia
+//
+//  Created by Matt Maher on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct hp_triviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
